8be182070b9c174c107af7fced645e413b834c8a
hugo ferreira <hugoytbrg.2002@gmail.com>
2019-11-15 14:57:49 +0000

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace chatbot
+{
+    class Program
+
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Olá humano, tudo bem?");
+            Console.WriteLine("\ts - Sim");
+            Console.WriteLine("\tn - Não");
+            Console.Write("Qual é a sua opçao? ");
+
+            switch (Console.ReadLine())
+            {
+                case "s":
+                    Console.WriteLine("Fico feliz, qual é a tua linguagem de programação favorita?");                                      
+                    break;
+                case "n":
+                    Console.WriteLine("É lidar!");
+                    break;
+
+            }
+
+        }
+    }
+}
